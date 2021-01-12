8b5f03375b3f8f57607e59aa54ee071b2f39fab3
tah96 <tylerandrewhunt96@gmail.com>
2021-01-11 20:45:11 -0500

    html & js merge

--- TraEdExp_AJ/Dashboard/static/app.js
+++ TraEdExp_AJ/Dashboard/static/app.js
@@ -20,22 +20,17 @@ function optionChanged() {
     d3.json("jobtitles.json").then(function (data) {
         var jobs = data
         var search = jobs.filter(job => job.Title == title);
-        console.log(search);
         var code = search[0].Code;
         search_code.push(code);
-        console.log(code);
     });
-    console.log(search_code);
 
     //Filter using the code for training, education & experience info using the train_edu_exp.json file.
     //Display in Career Snapshot panel.
     //Display as a donut graph 
     d3.json("train_edu_exp.json").then(function (data2) {
-        console.log(data2);
         var edu_tra_exp = data2;
         var ete = edu_tra_exp.filter(info => info.Code == search_code);
         var ete_info = ete[0];
-        console.log(ete_info);
         var eteinfo = d3.select("ul");
         eteinfo.html("");
         var education = d3.select('ul').append('li').text(`Typical education needed for entry: ${ete_info.Education}`);
@@ -66,21 +61,17 @@ function optionChanged() {
     //Display Median Salary in Career Snapshot panel.
 
     d3.json("Salary.json").then(function (data3) {
-        console.log(data3);
         var salary = data3;
         var median = salary.filter(info2 => info2.Code == search_code);
         var median_salary = median[0];
-        console.log(median_salary);
         var median_salary_info = d3.select("ul");
         var salary = d3.select('ul').append('li').text(`Median Annual Salary: $${median_salary.Median_Annual_Wage}`);
     });
 
     d3.json("national_emp_data.json").then(function (data4) {
-        console.log(data4);
         var salary_range = data4;
         var range = salary_range.filter(info3 => info3.Occupation_Code == search_code);
         var final_salary = range[0];
-        console.log(final_salary);
 
         //Graphed select Avg National Salary Data as a box plot.
         var boxchart = [{
@@ -100,13 +91,40 @@ function optionChanged() {
         };
         Plotly.newPlot('box', boxchart, layout2);
     });
+
+    var api_url = `https://api.careeronestop.org/v1/jobsearch/wuRO5lcrwDHuOce/${title}/${locationValue}/10/0/0/1/5/30`
+    console.log(api_url)
+
+    d3.json(api_url, {
+        headers: new Headers({
+            "Authorization": `Bearer blHjSxjGR1HbqZLw4GecmPnE+VuFzxX/zJmndSPV9JxvS8InRefDueufUtVCnUs2tH6n/sJDNFBhw+1dgM5oSA==`
+          })
+    }).then(function(cos_data) {
+        console.log(cos_data);
+        var job_listings = cos_data.Jobs;
+        console.log(job_listings);
+        var cos_html = d3.select("#cos-api")
+        cos_html.html("")
+        job_listings.forEach(job => {
+            console.log(job);
+            var job_title = job.JobTitle;
+            var job_company = job.Company;
+            var job_posting_date = job.AccquisitionDate;
+            var job_url = job.URL;
+            var job_location = job.Location
+            var job_box = cos_html.append('ul').html(`<b><u>${job_title}</u></b>`);
+            job_box.append('li').text(`Company: ${job_company}`);
+            job_box.append('li').text(`Location: ${job_location}`);
+            job_box.append('li').text(`Posting Date: ${job_posting_date}`);
+            job_box.append('li').html(`<a href=${job_url}>${job_url}</a>`);
+        });
+    });
 };
 
 function init() {
     d3.json("jobtitles.json").then(function (data) {
         for (var i = 0; i < data.length; i++) {
             var option = d3.select("#jobDataset").append("option").text(data[i].Title);
-            console.log(option);
         }
         var loading_job = data[0].Title
         console.log(loading_job)
